4e2ab1fda08da4e574c44a66adb37e18b31126f3
Goodman Design <124188979+ReedGoodman415@users.noreply.github.com>
2023-09-07 06:00:48 +0000

    V_8

---
+++ .js
@@ -0,0 +1,6 @@
+// This JavaScript code is not necessary, but it can be used to center the text in the header and footer.
+window.onload = function() {
+    document.querySelector("header").style.textAlign = "center";
+    document.querySelector("footer").style.textAlign = "center";
+  }
+  
